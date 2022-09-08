6704c86a4f83d7b815a706421254176f946aa91b
Taskin Mehereen <63733719+taskin-ms@users.noreply.github.com>
2022-09-08 21:40:48 +0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Binomial_Dist',
+      version='0.2',
+      description='Binomial distributions',
+      packages=['Binomial_Dist'],
+      author = 'Taskin Mehereen',
+      author_email = 'mehereen256@gmail.com',
+      zip_safe=False)
